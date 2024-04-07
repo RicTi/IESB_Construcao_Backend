048dac2663aae887101de800388ef32848da7e47
Ricardo Alves <motoric750@gmail.com>
2024-04-07 15:29:14 -0300

    Comitando alteracoes e requisicoes em primeira api...

--- 01-primeira-api/index.js
+++ 01-primeira-api/index.js
@@ -4,10 +4,56 @@ const express = require('express');
 // Criando a aplicação express...
 const app = express();
 
-// Middlewares
+// Middlewares -> Intermediários...
+// Imprime a data de quando foi feita a requisição.
+app.use((req, res, next) => {
+    const data = new Date().toISOString()
+    console.log(`Data: ${data}`)
+    next()
+})
 
+// Transformando o intermediário que transforma o corpo da requisição em JSON...
+// app.use(express.json())
 
 // Construir a lógica da minha API, que seria o contrato da minha API...
+app.get('/', (req, res) => {
+    res.send("Porta de resposta 1 funcionando corretamente...")
+})
+
+app.get("/nome", (req, res) => {
+    res.send("Ricardo A.!")
+})
+
+app.post('/nome', (req, res) => {
+    console.log(req.body)
+    res.json()
+})
+
+app.get('/aluno', (req, res) => {
+    res.send("Aluno: Ricardo Vinícius - Matrícula: 22109758")
+})
+
+// PATH PARAM pessoa/gustavo/20
+app.get('/pessoa/:nome/:idade', (req, res) => {
+    console.log(req.params)
+    const nomePessoa = req.params.nome
+    const idadePessoa = req.params.idade
+    let maiorDe18 = null
+    if (idadePessoa >= 18 ) {
+        maiorDe18 = "Maior de idade!"
+    } else {
+        maiorDe18 = "Menor de idade!"
+    }
+
+    res.send(`Olá, ${nomePessoa}. Você está bem? Você é ${maiorDe18}`)
+})
+
+
+
+
+
+
+
 
 
 
