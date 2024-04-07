9e4f21baf61db5f5d1df347b4283d0a51dfb62ca
Ricardo Alves <motoric750@gmail.com>
2024-04-07 11:35:21 -0300

    salvando segundo commit de hoje...

---
+++ 01-primeira-api/index.js
@@ -0,0 +1,18 @@
+// Importando o módulo express...
+const express = require('express');
+
+// Criando a aplicação express...
+const app = express();
+
+// Middlewares
+
+
+// Construir a lógica da minha API, que seria o contrato da minha API...
+
+
+
+
+// Definindo o start da api e a porta na qual será executada dentro do servidor...
+app.listen(3000, () => {
+    console.log("API iniciada... Rodando em http://localhost:3000")
+})
