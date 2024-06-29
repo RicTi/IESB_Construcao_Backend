b74e72b16936021c987de3a4cd2b4596042a9110
Ricardo Alves <motoric750@gmail.com>
2024-06-29 16:31:29 -0300

    conexão com o banco configurada!

--- api-teste-app/app.js
+++ api-teste-app/app.js
@@ -12,11 +12,16 @@ app.get('/', (req, res) => {
     res.status(200).json({ mensagem: "Bem vindo a nossa api teste! Este acesso é somente para visitantes."});
 });
 
-// Credenciais de acesso.
+//
 
+// Credenciais de acesso.
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_HOST = process.env.DB_HOST;
+const DB_NAME = process.env.DB_NAME;
 
 // Conexão com o Banco de dados.
-mongoose.connect()
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`)
 .then(() => {
     app.listen(3000)
     console.log("Conectado ao Banco!")
